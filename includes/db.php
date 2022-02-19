80e2acf7fb8416e6b2041e08a819551eb2906cbf
teamie <timmieprince@gmail.com>
2022-02-20 00:40:37 +0800

    db updated

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,10 @@
 <?php
-$db_username = 'localhost';
-$db_host = 'root';
-$db_password = '';
-$db_name = 'bookstore';
+$db_username = 'u785212331_bookstore_host';
+$db_host = 'localhost';
+$db_password = '#Bookstore2022';
+$db_name = 'u785212331_bookstore';
 
-$connection = mysqli_connect($db_username, $db_host, $db_password, $db_name);
+$connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if(!$connection){
   die("Connection Failed");
 }
