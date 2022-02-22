d6575340d529284399146507f7a25b308f1fa790
teamie <timmieprince@gmail.com>
2022-02-22 15:05:11 +0800

    dabase switched on

--- includes/db.php
+++ includes/db.php
@@ -1,26 +1,26 @@
 <?php
-// $db_username = 'u785212331_bookstore_host';
-// $db_host = 'localhost';
-// $db_password = '#Bookstore2022';
-// $db_name = 'u785212331_bookstore';
+$db_username = 'u785212331_bookstore_host';
+$db_host = 'localhost';
+$db_password = '#Bookstore2022';
+$db_name = 'u785212331_bookstore';
 
-// $connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-// if(!$connection){
-//   die("Connection Failed");
-// }
+$connection = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+if(!$connection){
+  die("Connection Failed");
+}
 ?>
 
 
 <?php
 // LOCAL DB for Modifications
 
-$db_username = 'localhost';
-$db_host = 'root';
-$db_password = '';
-$db_name = 'bookstore';
-$connection = mysqli_connect($db_username, $db_host, $db_password, $db_name);
-if(!$connection){
-die("Connection Failed");
-}
+// $db_username = 'localhost';
+// $db_host = 'root';
+// $db_password = '';
+// $db_name = 'bookstore';
+// $connection = mysqli_connect($db_username, $db_host, $db_password, $db_name);
+// if(!$connection){
+// die("Connection Failed");
+// }
 
 ?>
